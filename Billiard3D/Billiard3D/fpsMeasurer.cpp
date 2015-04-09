d98db8178ac10ec3a0354ca2acc5bdde92c6e72f
aglab2 <aglab3@gmail.com>
2015-04-09 08:43:49 +0000

    FPS: Начало сохранений

--- Billiard3D/Billiard3D/fpsMeasurer.cpp
+++ Billiard3D/Billiard3D/fpsMeasurer.cpp
@@ -2,10 +2,21 @@
 
 #define CURTIME std::chrono::system_clock::now()
 
-void fpsMeasure(Table &t){
-    //int numballs = t.balls.size();
+void fpsTest(std::vector<Ball> &balls, Table &t, double mt){
+    for (Ball& b: balls)
+        b.BoardCollide(t);
 
-    /*std::cout << "Start fps measurements!" << std::endl;
+    for (auto it = balls.begin(); it != balls.end(); ++it)
+        for (auto jt = it + 1; jt != balls.end(); ++jt)
+            it->Collide(t, *jt);
+
+    for (Ball& b: balls)
+        b.NextStep(t, mt);
+}
+
+
+int fpsCount(int numballs, Table t){
+    std::cout << "Start fps measurements!" << std::endl;
 
     //CLOCK binary search
     int l = 1;
@@ -19,32 +30,35 @@ void fpsMeasure(Table &t){
         std::vector<Ball> balls;
 
         for(int i = 0; i < numballs; i++){
-            balls.push_back(Ball("ball.cfg", vec(rand(), rand(), rand()), quat(1, vec(0, 0, 0)), vec(rand(), rand(), rand()), vec(rand(), rand(), rand())));
+            balls.push_back(Ball("ball.cfg", vec(rand(), rand(), rand()), quat(1, vec(0, 0, 0)), vec(rand(), rand(), rand()), vec(rand(), rand(), rand()), ""));
         }
 
         auto start = CURTIME;
         int i;
 
         for (i = 0; i < m && ((std::chrono::duration<double>)(CURTIME-start)).count() < 1; i++){
-            for (Ball& b: balls)
-                b.BoardCollide(t);
-
-            for (auto it = balls.begin(); it != balls.end(); ++it)
-                for (auto jt = it + 1; jt != balls.end(); ++jt)
-                    it->Collide(t, *jt);
-
-            for (Ball& b: balls)
-                b.NextStep(t, mt);
+            fpsTest(balls, t, mt);
         }
         if (i == m){
             l = m;
         }else{
             r = m;
         }
-    }*/
+    }
+    return (l+r)/2;
+}
 
-    double l = 1;
-    double r = 3000000;
-    t.CLOCK = (l+r)/2/t.FPS;
-    t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+void fpsMeasure(Table &t){
+    std::ifstream file("fps.cfg");
+    if (!file.is_open())
+    {
+        int numballs = t.balls.size();
+
+        t.CLOCK = fpsCount(numballs, t)/t.FPS;
+        t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+    }else{
+        if(file >> t.CLOCK >> t.MINTIME){
+            std::cout << "Your fps file is bad, I have to retest";
+        }
+    }
 }
