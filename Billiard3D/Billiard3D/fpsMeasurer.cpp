05c215931e2c616d4801e0c589a81cc3b59e93f9
aglab2 <aglab3@gmail.com>
2015-04-10 11:05:04 +0000

    FPS: Сохранения (1/2)

--- Billiard3D/Billiard3D/fpsMeasurer.cpp
+++ Billiard3D/Billiard3D/fpsMeasurer.cpp
@@ -52,13 +52,77 @@ void fpsMeasure(Table &t){
     std::ifstream file("fps.cfg");
     if (!file.is_open())
     {
+        std::cout << "No fps file found, measuring..." << std::endl;
         int numballs = t.balls.size();
 
         t.CLOCK = fpsCount(numballs, t)/t.FPS;
         t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+
+        std::ofstream file_out("fps.cfg");
+        file_out << t.CLOCK;
     }else{
-        if(file >> t.CLOCK >> t.MINTIME){
-            std::cout << "Your fps file is bad, I have to retest";
+        if(!(file >> t.CLOCK)){
+            std::cout << "Your fps file is bad, I have to retest" << std::endl;
+            int numballs = t.balls.size();
+            t.CLOCK = fpsCount(numballs, t)/t.FPS;
+            t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+
+            std::ofstream file_out("fps.cfg");
+            file_out << t.CLOCK;
+        }else{
+            int numballs = t.balls.size();
+            std::cout << "OK, I found your cfg. Let me check it" << std::endl;
+
+            int m = t.CLOCK * t.FPS;
+            std::cout << "Trying " << m << " ticks" << std::endl;
+            double mt = 1/t.SLOWFACTOR/m;
+
+            std::vector<Ball> balls;
+
+            for(int i = 0; i < numballs; i++){
+                balls.push_back(Ball("ball.cfg", vec(rand(), rand(), rand()), quat(1, vec(0, 0, 0)), vec(rand(), rand(), rand()), vec(rand(), rand(), rand()), ""));
+            }
+
+            auto start = CURTIME;
+            int i;
+            double time;
+
+            for (i = 0; i < m && (time = ((std::chrono::duration<double>)(CURTIME-start)).count()) < 1.15; i++){
+                fpsTest(balls, t, mt);
+            }
+
+            if (i == m){
+                if (time > 0.85){
+                    std::cout << "OK, I believe you..." << std::endl;
+                    t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+                }else{
+                    std::cout << "Hmm, your measurements are too inaccurate!" << std::endl;
+                    std::cout << "Shall I remeasure it for you? [Y/n] ";
+
+                    char c;
+                    std::cin >> c;
+                    if (c == 'n' || c == 'N'){
+                        std::cout << "As you wish..." << std::endl;
+                        t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+                        return;
+                    }else{
+                        std::cout << "Good choice!" << std::endl;
+                        int numballs = t.balls.size();
+                        t.CLOCK = fpsCount(numballs, t)/t.FPS;
+                        t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+                        std::ofstream file_out("fps.cfg");
+                        file_out << t.CLOCK;
+                    }
+                }
+            }else{
+                std::cout << "You lied me! I hate you, have to remeasure it!" << std::endl;
+                int numballs = t.balls.size();
+                t.CLOCK = fpsCount(numballs, t)/t.FPS;
+                t.MINTIME = 1/t.SLOWFACTOR/t.CLOCK;
+
+                std::ofstream file_out("fps.cfg");
+                file_out << t.CLOCK;
+            }
         }
     }
 }
