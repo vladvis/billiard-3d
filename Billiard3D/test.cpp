cad842c72b817a5614a50706fc97fd7ab142e6af
aglab2 <aglab3@gmail.com>
2015-05-09 22:54:31 +0300

    CLion base!

--- Billiard3D/test.cpp
+++ Billiard3D/test.cpp
@@ -1,17 +1,10 @@
 #include "vec.h"
-#include "table.h"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <iomanip>
-#include <pthread.h>
 #include "glutRender.h"
-
-
 
 using namespace std;
 
 int main(int argc, char * argv[])
 {
 	glutRender::Instance.Init(&argc, argv, "table.cfg", "ball.cfg", "sounds.cfg");
+	return 0;
 }
